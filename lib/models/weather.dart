95c14fb0bda15a9d0c81e89a10db59bc19d85960
nireinhard <nireinhard@htwsaar.de>
2019-01-02 09:53:14 +0100

    initial commit

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:weather/widgets/utility/converter.dart';
+import 'package:tuple/tuple.dart';
+import 'dart:math';
 
 part 'weather.g.dart';
 
 enum WeatherStatus{
-  SUNNY, CLOUDY, RAINY
+  SUNNY, CLOUDY, RAINY, STORMY, SNOWY
+}
+
+class ForecastData{
+  final int timeStamp;
+  final String temp;
+  final WeatherStatus status;
+  
+  String get getTimeStamp => Converter.timeStamp2Readable(timeStamp);
+
+  ForecastData(this.timeStamp, this.temp, this.status);
+}
+
+class WeekdayForecast{
+  final int weekday;
+  final int timeStamp;
+  final WeatherStatus status;
+  List<int> temps;
+
+  int get getWeekday => DateTime.fromMillisecondsSinceEpoch(timeStamp*1000).weekday;
+  int get minTemp => temps.isNotEmpty ? temps.reduce(min) : 0;
+  int get maxTemp => temps.isNotEmpty ? temps.reduce(max) : 0;
+
+  WeekdayForecast(this.weekday, this.timeStamp, this.status){
+    this.temps = [];
+  }
 }
 
 @JsonSerializable()
@@ -12,10 +40,26 @@ class Weather {
   final int id;
   final String title;
   final String countryCode;
-  final String temp;
-  final WeatherStatus status;
+  String temp;
+  WeatherStatus status;
+  Map<String, dynamic> stats;
+  List<ForecastData> todayforecast;
+  List<WeekdayForecast> upcomingForecast;
 
-  Weather({@required this.id, @required this.title, @required this.countryCode, this.temp, this.status});
+  Weather({@required this.id, @required this.title, @required this.countryCode, this.temp, this.status}){
+    this.stats = {};
+    this.todayforecast = [];
+    this.upcomingForecast = [];
+  }
+
+  void addWeekdayForecast(int weekday, int timeStamp, WeatherStatus status, int temp){
+    List<WeekdayForecast> filtered = upcomingForecast.where((entry) => entry.getWeekday == weekday).toList();
+    if(filtered.length >= 1){
+      filtered[0].temps.add(temp);
+      return;
+    }
+    upcomingForecast.add(WeekdayForecast(weekday, timeStamp, status));
+  }
 
   factory Weather.fromJson(Map<String, dynamic> json) => _$WeatherFromJson(json);
 
--- lib/scoped-models/weather_model.dart
+++ lib/scoped-models/weather_model.dart
@@ -1,32 +1,106 @@
 import 'package:scoped_model/scoped_model.dart';
 import 'package:weather/models/weather.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:weather/widgets/utility/converter.dart';
+import 'package:weather/widgets/utility/weather.dart';
+import 'package:localstorage/localstorage.dart';
 
-class WeatherModel extends Model{
-  List<Weather> _weatherList = [
-    Weather(id: 1, title: "Berlin", countryCode: 'DE', status: WeatherStatus.SUNNY, temp: "30°C"),
-    Weather(id: 2, title: "Saarbrücken", countryCode: 'DE', status: WeatherStatus.CLOUDY, temp: "30°C")
-  ];
-  
-  List<Weather> get weatherList{
+class WeatherModel extends Model {
+  List<Weather> _weatherList = [];
+
+  _getDataFromApi(Weather w) async {
+    http.Response weatherResponse = await http.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=${w.title},${w.countryCode}&APPID=bcce0e3e45b54cf1c6d55eeb1be2307d");
+    http.Response forecastResponse = await http.get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=${w.title},${w.countryCode}&APPID=bcce0e3e45b54cf1c6d55eeb1be2307d");
+    Map<String, dynamic> weatherData = json.decode(weatherResponse.body);
+    Map<String, dynamic> forecastData = json.decode(forecastResponse.body);
+    print(weatherData);
+    w.status =
+        WeatherUtility.parseWeatherStatusCode(weatherData['weather'][0]['id']);
+    w.stats.putIfAbsent("sunrise",
+        () => Converter.timeStamp2Readable(weatherData["sys"]["sunrise"]));
+    w.stats.putIfAbsent("sunset",
+        () => Converter.timeStamp2Readable(weatherData["sys"]["sunset"]));
+    w.stats.putIfAbsent("sunsettimestamp", () => weatherData["sys"]["sunset"]);
+    w.stats.putIfAbsent("humidity", () => weatherData["main"]["humidity"]);
+    w.stats.putIfAbsent("pressure", () => weatherData["main"]["pressure"]);
+    w.stats.putIfAbsent("clouds", () => weatherData['clouds']['all']);
+    w.temp = Converter.k2Celsius(weatherData['main']['temp']);
+
+    // remove all items from list, as this method is called to refresh data
+    // and therefore would put in items multiple times when not clearing the list before adding
+    w.todayforecast.removeWhere((_) => true);
+
+    forecastData['list'].forEach((forecast) {
+      var today = DateTime.now();
+      if (DateTime.fromMillisecondsSinceEpoch(forecast['dt'] * 1000).day ==
+          today.day) {
+        ForecastData forecastObject = ForecastData(
+          forecast['dt'],
+          Converter.k2Celsius(forecast['main']['temp']),
+          WeatherUtility.parseWeatherStatusCode(forecast['weather'][0]['id']),
+        );
+        w.todayforecast.add(
+          forecastObject,
+        );
+      } else {
+        w.addWeekdayForecast(
+            DateTime.fromMillisecondsSinceEpoch(forecast['dt'] * 1000).weekday,
+            forecast['dt'],
+            WeatherUtility.parseWeatherStatusCode(forecast['weather'][0]['id']),
+            Converter.k2CelsiusRounded(forecast['main']['temp']));
+      }
+    });
+
+    notifyListeners();
+  }
+
+  List<Weather> get weatherList {
     // returns a new copy of the list
-    return _weatherList.toList();
+    return _weatherList;
   }
 
-  loadWeatherList(){
-
+  Future<void> refreshWeatherList() {
+    return Future.forEach(_weatherList, (Weather w) => _getDataFromApi(w));
   }
 
-  _persistWeatherList(){
-     // persists current weather list to disk
+  Future<void> loadWeatherList() async {
+    final LocalStorage _storage = LocalStorage('weather_app');
+    await _storage.ready;
+    List list = await _storage.getItem("weatherlist");
+    List<Weather> decodedList = list.map<Weather>((json) {
+      Weather w = Weather.fromJson(jsonDecode(json));
+      return w;
+    }).toList();
+    _weatherList = decodedList;
+    await refreshWeatherList();
+    notifyListeners();
   }
 
-  void addWeather(Weather w){
+  Future<void> _persistWeatherList() async {
+    final LocalStorage _storage = LocalStorage('weather_app');
+    await _storage.ready;
+    List<String> jsonList =
+        _weatherList.map<String>((Weather w) => jsonEncode(w)).toList();
+    await _storage.setItem("weatherlist", jsonList);
+  }
+
+  void addWeather(Weather w) {
     _weatherList.add(w);
+    _loadWeatherData(w);
     _persistWeatherList();
     notifyListeners();
   }
 
-  void removeWeather(int index){
+  void _loadWeatherData(Weather w) {
+    _getDataFromApi(w);
+    notifyListeners();
+  }
+
+  void removeWeather(int index) {
     _weatherList.removeAt(index);
     _persistWeatherList();
     notifyListeners();
